5b03e454c1418e1b5d4dafa3f63d41573dc6eb30
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-08-19 15:37:40 -0400

    Create url.js

---
+++ url.js
@@ -0,0 +1,15 @@
+if(window.location.href == "https://lucky2245.github.io/website/index.html"){
+  window.location.assign("index");
+}
+if(window.location.href == "https://lucky2245.github.io/website/page2.html"){
+  window.location.assign("page2");
+}
+if(window.location.href =="https://lucky2245.github.io/website/about.html"){
+  window.location.assign("about");
+}
+if(window.location.href =="https://lucky2245.github.io/website/404.html"){
+  window.location.assign("404");
+}   
+if(window.location.href == "https://lucky2245.github.io/page3.html"){
+  window.location.assign("page3");
+}
