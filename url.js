53c50b330f10808ca6fb648504ab1cda08254d13
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-08-21 16:46:34 -0400

    Update url.js

--- url.js
+++ url.js
@@ -1,15 +1,25 @@
+
+//Change index.html index
 if(window.location.href == "https://lucky2245.github.io/website/index.html"){
   window.location.assign("index");
 }
+//change page2.html to page 2
 if(window.location.href == "https://lucky2245.github.io/website/page2.html"){
   window.location.assign("page2");
 }
+//change about.html to about
 if(window.location.href =="https://lucky2245.github.io/website/about.html"){
   window.location.assign("about");
 }
+//change 404.html to 404
 if(window.location.href =="https://lucky2245.github.io/website/404.html"){
   window.location.assign("404");
-}   
+}  
+//change page4.html to page4
 if(window.location.href == "https://lucky2245.github.io/website/page3.html"){
   window.location.assign("page3");
+}
+//change page4.html to page4
+if(window.location.href == "https://lucky2245.github.io/website/404.html"){
+  window.location.assign("404");
 }
