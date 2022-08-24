bc9038e7dc8539cca6185c39cea59afc18472710
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-08-24 14:07:55 -0400

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,32 +1,33 @@
-function setCookie(cname,cvalue,exdays) {
+function setCookie(cname, cvalue, exdays) {
   const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires=" + d.toUTCString();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
+
 function getCookie(cname) {
   let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
+  let ca = document.cookie.split(';');
   for(let i = 0; i < ca.length; i++) {
     let c = ca[i];
     while (c.charAt(0) == ' ') {
       c = c.substring(1);
     }
-    if(c.indexOf(name) == 0){
+    if (c.indexOf(name) == 0) {
       return c.substring(name.length, c.length);
     }
   }
   return "";
 }
-function checkCookie(){
+
+function checkCookie() {
   let user = getCookie("username");
-  if(user != ""){
-    alert("welcome again " + user + " . The signup button will still show");
-  }else{
-    user = document.getElementById("signuppage").value;                    
-    if(user != "" && user != null){
-      setCookie("username", user, 30);
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+    user = document.getElementById("signuppage").value;    
+    if (user != "" && user != null) {
+      setCookie("username", user, 365);
     }
   }
 }
